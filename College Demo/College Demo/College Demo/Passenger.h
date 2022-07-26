e0fdb4082477e88797ef874268ef619cc49c719e
dawndone <109032485+dawndone@users.noreply.github.com>
2022-07-26 20:56:42 +0800

    work3

---
+++ College Demo/College Demo/College Demo/Passenger.h
@@ -0,0 +1,42 @@
+//
+//  Passenger.h
+//  College Demo
+//
+//  Created by mashujun on 2022/7/18.
+//
+
+#import "Person.h"
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface Orders : NSObject
+
+@property (nonatomic, copy) NSDate *starttime;
+@property (nonatomic, copy) NSDate *endtime;
+@property (nonatomic, copy) NSString *fromwhere;
+@property (nonatomic, copy) NSString *desitination;
+@property (nonatomic, copy) float price;
+@property (nonatomic, copy) NSString *number;
+
+- (void)setorder:starttime:(NSDate *)starttime endtime:(NSDate *)endtime fromwhere:(NSString *)fromwhere
+ destination:(NSString *)destination price:(float)price number:(NSString *)number;
+
+@end
+
+@interface Passenger : Person
+// @property 属性
+// 是否年满 18 岁
+@property (nonatomic, copy) bool ifeighteen;
+// 历史订单 （数组）
+@property (nonatomic, copy) NSMutableArray *historders = nil;
+// 未出行订单 （数组）
+@property (nonatomic, copy) NSMutableArray *notdoneorders = nil;
+
+// Function 方法
+// 去订票
+- (void)booktickets:(Orders *)neworder;
+// 去检票
+- (void)booktickets:(Orders *)doneorder;
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ College Demo/College Demo/College Demo/Passenger.m
@@ -0,0 +1,34 @@
+//
+//  Passenger.m
+//  College Demo
+//
+//  Created by mashujun on 2022/7/18.
+//
+
+#import "Passenger.h"
+
+
+@implementation Orders
+
+- (void)setorder:starttime:(NSDate *)starttime endtime:(NSDate *)endtime fromwhere:(NSString *)fromwhere
+ destination:(NSString *)destination price:(float)price number:(NSString *)number{
+    self->starttime = starttime;self->endtime = endtime;self->fromwhere = fromwhere;self->destination = destination;
+    self->price = price;self->number=number;
+ }
+
+@end
+
+@implementation Passenger
+
+- (void)booktickets:(Orders *)neworder {
+    [historders addObject:@"neworder"];
+    [notdoneorders addObject:@"neworder"];
+}
+
+- (void)booktickets:(Orders *)doneorder {
+    Orders *newdone = [[Orders alloc] init];
+    newdone = doneorder;
+    [notdoneorders removeObject:@"newdone"];
+}
+
+@end
